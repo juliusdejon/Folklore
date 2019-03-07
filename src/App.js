888d5c908b8bd3221a18f7a09a27eec46432502e
Julius Dejon <julius.dejon@shoresuite.com>
2019-03-07 11:25:28 +0800

    Loading Data plain react

--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import Spinner from 'react-spinkit';
+
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      posts: [],
+      isLoading: false,
+    };
+  }
+
+  componentDidMount(){
+    this.setState({ isLoading: true });
+
+    fetch("https://jsonplaceholder.typicode.com/posts").then(response => response.json()).then(data => {
+      this.setState({ posts: data,
+        isLoading: false })
+    }
+   )
+  }
+
   render() {
+    const { posts, isLoading } = this.state;
+    console.log(posts)
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <img src={logo} className="App-logo" alt="logo" />
+        <div className="App-main">
+        <ul>
+          { !isLoading ? (<Spinner name="ball-pulse-sync" />) :
+            (posts.map(post =>  
+            <li key={post.id}>
+              {post.title}
+            </li>)
+            )
+          }
+        </ul>
+        </div>
       </div>
     );
   }
